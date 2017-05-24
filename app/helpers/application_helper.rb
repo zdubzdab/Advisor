3a8f979f260baa1b0f71446cc2281904736bc827
zdubzdab <zdubzdab@gmail.com>
2017-05-24 12:11:33 +0300

    Add header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def hide_link_to_main_page(link)
+    link unless current_page?('/')
+  end
+
+  def show_link_depend_on_user_role(logout, login, signup)
+    if current_user
+      logout
+    else
+      hide_link_on_some_pages(login, signup)
+    end
+  end
+
+  private
+
+    def hide_link_on_some_pages(login, signup)
+      if request.original_url.split('/').last == 'sign_in'
+        signup
+      else
+        login
+      end
+    end
 end
